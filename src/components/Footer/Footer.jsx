04c891095b1308d048ea32e444a68695d5589187
Angelique Precheur <precheur.angelique@gmail.com>
2023-03-20 17:22:37 +0100

    modif. Footer + Ajustement page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,13 +6,10 @@ import './Footer.css';
 
 function Footer() {
   return (
-    <footer>
-      <Link to={Home}>
-        <img src={LogoDrdrone} alt="logo enseigne" className='LogoDrDrone'/>
-      </Link>
-      <div>
+    <footer className="wrapper">
+      <div className="TextFooterLink wrapper1">
         <a
-          className="footerlinks" // pour rappeler les classes de style.css
+          className="footerlinks GridTextFooter1" // pour rappeler les classes de style.css
           href="https://drdrone.fr/#services"
           rel="noreferrer"
           target="_blank" // permet quand clique sur le lien, d'ouvrir une nouvelle page !
@@ -20,7 +17,7 @@ function Footer() {
           Nos prestations
         </a>
         <a
-          className="footerlinks"
+          className="footerlinks GridTextFooter2"
           href="https://drdrone.fr/qui-sommes-nous/"
           rel="noreferrer"
           target="_blank"
@@ -28,7 +25,7 @@ function Footer() {
           Qui sommes nous ?
         </a>
         <a
-          className="footerlinks"
+          className="footerlinks GridTextFooter3"
           href="https://drdrone.fr/demande-de-devis/"
           rel="noreferrer"
           target="_blank"
@@ -36,7 +33,7 @@ function Footer() {
           Demande de devis
         </a>
         <a
-          className="footerlinks"
+          className="footerlinks GridTextFooter4"
           href="https://drdrone.fr/contact/"
           rel="noreferrer"
           target="_blank"
@@ -44,13 +41,19 @@ function Footer() {
           Contact
         </a>
         <a
-          className="footerlinks"
+          className="footerlinks GridTextFooter5"
           href="https://drdrone.fr/mentions-legales/"
           rel="noreferrer"
           target="_blank"
         >
           Mentions légales
         </a>
+      </div>
+      <Link to={Home} className="Logofooter LogoGrid">
+        <img src={LogoDrdrone} alt="logo enseigne" />
+      </Link>
+      <div className="Copyright FooterOnFooter">
+        © Copyright
       </div>
     </footer>
   );
