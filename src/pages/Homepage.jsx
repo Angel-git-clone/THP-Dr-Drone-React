75bdf0dba6fb464ab7bf3cb0483c3cfbb8957b53
Angelique Precheur <precheur.angelique@gmail.com>
2023-03-21 16:52:22 +0100

    Shaping the Text and Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import Grid from '@mui/system/Unstable_Grid';
 import AuthenForm from '../components/AuthForm/AuthenForm';
+import '../pages/Homepage.css';
 
 export default function homepage() {
   return (
     <Grid container columns={2}>
-      <Grid item xs={1}>
-        <h1>Dr. Drone :</h1>
-        <h3>" Merci de faire partie de l’équipe ! "</h3>
+      <Grid item xs={1} className="TextLeft">
+        <div>
+          <h1>Dr. Drone :</h1>
+          <h3>" Merci de faire partie de l’équipe ! "</h3>
+        </div>
       </Grid>
       <Grid item xs={1}>
         <AuthenForm />
