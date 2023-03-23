621a5e49dfd097db50acb6ef9b64ecd903ebb2ba
Angelique Precheur <precheur.angelique@gmail.com>
2023-03-23 22:22:02 +0100

    video search modif.

--- src/pages/Formations/Formations.jsx
+++ src/pages/Formations/Formations.jsx
@@ -6,8 +6,20 @@ export default function Formations() {
   return (
     <div>
       <span>
-        <p>Hello</p>
-        <video width="540" height='100%' src={Video} type="video/mp4" auto autoplay loop />
+        <div>
+          <h2>Title</h2>
+          <p>Hello</p>
+        </div>
+        <video 
+          autoplay 
+          loop 
+          muted 
+          width='540'
+          height='100%'
+          id='video'
+        >
+          <source src={Video} type="video/mp4" />
+        </video>
       </span>
       <span>
         <p>Helloooooo</p>
