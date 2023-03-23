38b6fcb69a7515c6f07b10812a9eac096359ac84
Angelique Precheur <precheur.angelique@gmail.com>
2023-03-23 19:06:57 +0100

    test "Formations"

--- src/pages/Formations/Formations.jsx
+++ src/pages/Formations/Formations.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import './Formations.css';
-import Video1 from '../../assets/trailer-Dr-drone-def.mp4';
+import Video from '../../assets/trailer-Dr-drone-def.mp4';
 
 export default function Formations() {
   return (
     <div>
-      <video controls width="250" height='500' src={Video1} type="video/mp4"></video>
+      <span>
+        <p>Hello</p>
+        <video width="540" height='100%' src={Video} type="video/mp4" auto autoplay loop />
+      </span>
+      <span>
+        <p>Helloooooo</p>
+      </span>
     </div>
   );
 }
