3a5b2aceab04d14d213abba97b1582b429c95ac7
Angelique Precheur <precheur.angelique@gmail.com>
2023-03-27 00:37:13 +0200

    Shaping Formations page

--- src/pages/Formations/Formations.jsx
+++ src/pages/Formations/Formations.jsx
@@ -2,10 +2,10 @@ import './Formations.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Video from '../../assets/trailer-Dr-drone-def.mp4';
-import Posts from '../posts';
+import Posts from '../Posts/posts';
 
-const API_URL = 'https://backend-drdrone.herokuapp.com/api/posts';
-// const API_URL = 'http://127.0.0.1:3000/api/posts'; //run in local
+// const API_URL = 'https://backend-drdrone.herokuapp.com/api/posts';
+const API_URL = 'http://127.0.0.1:3000/api/posts'; //run in local
 
 async function getAPIData() {
   return axios.get(API_URL, {
@@ -26,10 +26,10 @@ export default function Formations() {
   }, []);
 
   return (
-    <div>
-      <div>
-        <details open>
-          <summary>Nos formations</summary>
+    <div style={{ marginBottom: '4em' }}>
+      <div className="Presentation">
+        <details open className="Summary">
+          <summary style={{ color: 'deeppink', fontWeight: 'bold' }}>Nos formations</summary>
           <ul>
             <li><a href="#1"> Démousser un toit </a></li>
             <li><a href="#2"> Nid de frelon </a></li>
@@ -48,10 +48,29 @@ export default function Formations() {
           <source src={Video} type="video/mp4" />
         </video>
       </div>
-
-      <div>
-        <p>La liste des formations: </p>
-        <Posts posts={posts} />
+      <h3 style={{ color: 'black', textAlign: 'center' }}>
+        <stong style={{ color: 'deeppink' }}>Notre priorité : </stong>
+        Exercer avec le plus grand respect des normes de sécurité !
+      </h3>
+      <div className="wrapper1" style={{ marginTop: '2em', marginBottom: '2em' }}>
+        <div className="wrapper2">
+          <strong className="TitlePost">La liste des formations : </strong>
+          <div className="Posts">
+            <Posts posts={posts} />
+          </div>
+        </div>
+        <div className="CategoryPostGroup">
+          <strong>La liste des catégories : </strong>
+          <p className="CategoryPost">
+            Catégorie 1 : "Technique"
+          </p>
+          <p className="CategoryPost">
+            Catégorie 2 : "Commercial"
+          </p>
+          <p className="CategoryPost">
+            Catégorie 3 : "Administratif"
+          </p>
+        </div>
       </div>
     </div>
   );
---
+++ src/pages/Posts/posts.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './posts.css';
+
+function Posts(props) {
+  return (
+    <div>
+      {props.posts.map((post) => (
+        <div key={post.id} className="background" style={{ padding: '1em', margin: '1em'}}>
+          <div style={{ maxHeight: '7em' }}>
+            <p style={{ color: 'white', fontWeight: 'bold' }}>{post.title}</p>
+            <p>{post.content}</p>
+            <p>Il appartient à la catégorie numéro <strong style={{ color: 'white' }}>{post.category_id}</strong></p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Posts;
